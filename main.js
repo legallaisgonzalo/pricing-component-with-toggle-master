d343453282e470ba15e628c24ad73eefa8037107
gonza <gonzamendez18@gmail.com>
2020-11-03 13:50:56 -0300

    ultimos detalles

---
+++ main.js
@@ -0,0 +1,14 @@
+function toggleChange(event){
+  const basic = document.getElementById("basic");
+  const professional = document.getElementById("professional");
+  const master = document.getElementById("master");
+  if(event.checked === true){
+      basic.textContent = `$19.99`;
+      professional.textContent = `$24.99`;
+      master.textContent = `$39.99`; 
+  }else{
+      basic.textContent = `$199.99`;
+      professional.textContent = `$249.99`;
+      master.textContent = `$399.99`;
+  }    
+}
